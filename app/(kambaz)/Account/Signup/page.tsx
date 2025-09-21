9c94957a6b5bed3f482c2ef5c41d7f17f2ee3470
Rishika Reddy Vootkur <rishikareddyvootkur@Rishika-Reddys-MacBook.local>
2025-09-21 16:37:38 -0400

    third commit

--- app/(kambaz)/Account/Signup/page.tsx
+++ app/(kambaz)/Account/Signup/page.tsx
@@ -3,9 +3,9 @@ export default function Signup() {
     return (
         <div id="wd-signup-screen">
             <h3>Sign up</h3>
-            <input placeholder="john doe" className="wd-username"/><br/>
-            <input defaultValue="1234" type="password" className="wd-password"/><br/>
-            <input defaultValue="1234" type="password" className="wd-verify-password" /><br/>
+            <input defaultValue="john doe" placeholder="username" className="wd-username"/><br/>
+            <input defaultValue="1234" placeholder="password" type="password" className="wd-password"/><br/>
+            <input defaultValue="1234" placeholder="verify password" type="password" className="wd-verify-password" /><br/>
             <Link href="Profile">Sign up</Link><br/>
             <Link href="Signin">Sign in</Link>
         </div>
